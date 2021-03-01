986bd32596347e1a458de24dd46a58b70d56b276
Natalia Muszyńska <74049761+arbitralizm@users.noreply.github.com>
2021-03-01 11:22:59 +0100

    Add files via upload

---
+++ lab 0/lab 0 - zadania 1.2 - 12.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <windows.h>
+
+
+
+//ZADANIE 1.2.2
+/*int main()
+{
+    printf("Bardzo \n dlugi \n napis \n");
+    return 0;
+}*/
+
+//ZADANIE 1.2.3
+/*int main()
+{
+    printf("Napis zawierajacy rozne dziwne znaczki // \\ $ & % \n");
+    return 0;
+}*/
+
+//ZADANIE 1.2.4
+/*int main()
+{
+    printf("Wpisz liczbe calkowita: \n");
+    int x;
+    scanf("%d", &x);
+    printf("%d", x);
+    return 0;
+}*/
+
+//ZADANIE 1.2.6
+/*int main()
+{
+    int x, y, z;
+    printf("Wpisz po kolei 3 liczby calkowite: \n");
+    scanf("%d", &x);
+    printf("Twoje liczby:  %d", x "\n");
+    scanf("%d", &y);
+    printf("Twoje liczby:  %d", y "\n");
+    scanf("%d", &z);
+    printf("Twoje liczby:  %d", z "\n");
+    return 0;
+}*/
+
+// ZADANIE 1.2.7
+
+/*int main()
+{
+    int x;
+    int y = 1;
+    scanf("%d", &x);
+
+    int suma = (x+y);
+    printf("%d", suma(x,y));
+    return suma;
+}*/
+
+//zadanie 1.2.8
+/*int main()
+{
+    int x, y, z;
+    scanf("%d", &x);
+    scanf("%d", &y);
+    scanf("%d", &z);
+    printf("%f", (double)(x+y+z)/3 );
+    return 0;
+}*/
+
+//zadanie 1.2.9
+/*int main()
+{
+    float x;
+    scanf("%f", &x);
+    printf("%f", sqrt(x));
+    return 0;
+}*/
+
+//zadanie 1.2.10
+/*int main()
+{
+    float x;
+    scanf("%f", &x);
+    printf("%f", fabs(x));
+    return 0;
+}*/
+
+// zadanie 1.2.11
+/*int main()
+{
+    float x;
+    scanf("%f", &x);
+    printf("%.2f", x);
+    return 0;
+}*/
+
+// zadanie 1.2.12
+
+int main()
+{
+    float x;
+    scanf("%f", &x);
+    printf("\n %e", x);
+    return 0;
+}
+
+
