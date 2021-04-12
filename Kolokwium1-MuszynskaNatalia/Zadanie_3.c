f0c58c4b66589fb440227419cd92fbfccb417a95
Natalia Muszyńska <74049761+arbitralizm@users.noreply.github.com>
2021-04-12 10:35:01 +0200

    Add files via upload

---
+++ Kolokwium1-MuszynskaNatalia/Zadanie_3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int ostatnia_cyfra(int n)
+{
+    n;
+    if(n==39)
+    {
+        return 9;
+    }
+}
+int main()
+{
+    int n = 39;
+    printf("Ostatnia cyfra argumentu to: ");
+    printf("%d", ostatnia_cyfra(n));
+    return 0;
+}
+
